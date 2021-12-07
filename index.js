5311ae78b1f9af757dd1d637ee7c81bf015cc831
tovebr <tove.brandt@student.kyh.se>
2021-12-07 10:36:51 +0100

    landing page done

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 let slideIndex = 0;
 
 function slideShow() {
-  const slides = Array.from(document.getElementsByClassName("carousel-item"));
-
-  slides.forEach((slide) => (slide.style.display = "none"));
-
-  slideIndex = slideIndex > slides.length - 1 ? 1 : slideIndex + 1;
-
-  slides[slideIndex - 1].style.display = "block";
+  const images = Array.from(document.getElementsByClassName("carousel-item"));
+  images.forEach((item) => (item.style.display = "none"));
+  slideIndex = slideIndex > images.length - 1 ? 1 : slideIndex + 1;
+  images[slideIndex - 1].style.display = "block";
   setTimeout(slideShow, 5000);
 }
 
