ce05224f2eed8305d2468b0e2576bebae46a33a3
tovebr <tove.brandt@student.kyh.se>
2022-01-05 15:21:19 +0100

    can handle cart now

--- shop.js
+++ shop.js
@@ -3,6 +3,7 @@ const searchClear = document.querySelector(".clear");
 
 const articlesContainer = document.querySelector(".articles-display");
 let searchResult;
+
 let cart = { articles: [] };
 
 window.onscroll = function () {
@@ -39,18 +40,18 @@ function printCards(articlesArray) {
     const cardDiv = document.createElement("div");
     cardDiv.classList = "card";
     if (!article.id) article.id = i;
-    cardDiv.id = article.id;
+    if (!article.size) article.size = "big";
+    if (!article.finish) article.finish = "matte";
 
-    const title = casedWords(article.title);
-    const series = casedWords(article.series);
+    cardDiv.id = article.id;
 
     cardDiv.innerHTML = `<div class="img-container">
       <img src="${article.img}" alt="${article.title}">
     </div>
     <div class="info">
-      <h3 class="title">${title}</h3>
+      <h3 class="title">${casedWords(article.title)}</h3>
       <p><span class="category">Price: </span>${article.price} kr</p>
-      <p><span class="category">Series: </span>${series}</p>
+      <p><span class="category">Series: </span>${casedWords(article.series)}</p>
       <div class="btn-group">
 
         <button class="btn btn-buy">Buy</button>
@@ -114,10 +115,70 @@ document
   .forEach((el) => el.addEventListener("click", (e) => createModal(e)));
 document
   .querySelectorAll(".btn-buy")
-  .forEach((el) => el.addEventListener("click", (e) => addToCart(e)));
+  .forEach((el) =>
+    el.addEventListener("click", (e) =>
+      addToCart(e.target.parentNode.parentNode.parentNode.id)
+    )
+  );
+
+function cartList() {
+  cart.articles.sort((a, b) => {
+    if (a.title > b.title) {
+      return 1;
+    } else if (a.title < b.title) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+
+  let html = "";
+
+  if (cart.articles.length > 0) {
+    cart.articles.forEach((article) => {
+      html += `<li id="${
+        article.cartId
+      }"><div class="img-container"><img src="${
+        article.img
+      }"></div><h3 class="article-title">${casedWords(article.title)}</h3><div>
+      <label for="size">Size</label><select name="size" id="size" class="print-option">
+      <option value="big">70x90</option>
+      <option value="medium" ${
+        article.size === "medium" ? "selected" : ""
+      }>50x70</option>
+      <option value="small" ${
+        article.size === "small" ? "selected" : ""
+      }>30x20</option>
+    </select></div>
+    <div>
+    <label for="finish">Finish</label>
+    <select name="paper" id="paper" class="print-option">
+      <option value="matte" >Matte</option>
+      <option value="glossy" ${
+        article.finish === "glossy" ? "selected" : ""
+      }>Glossy</option>
+    </select></div><p class="article-price">${
+      article.price
+    } kr</p><i class="bi bi-trash"></i></li>`;
+    });
+  } else {
+    html += emptyCartMessage();
+  }
+  return html;
+}
+
+function emptyCartMessage() {
+  return `<li class="empty-message"><h2>Your cart is empty</h2></li>`;
+}
+
+function calcTotal() {
+  let sum = 0;
+  if (cart.articles.length > 0)
+    cart.articles.forEach((article) => (sum += article.price));
+  return sum;
+}
 
 function createModal(e) {
-  console.log(e.target);
   const position = window.scrollY;
 
   let modalWindow = document.createElement("div");
@@ -129,52 +190,31 @@ function createModal(e) {
   document.body.style.position = "fixed";
   document.body.appendChild(modalWindow);
 
-  e.target.parentNode.parentNode.parentNode.classList.contains("card") &&
+  let stored = "";
+
+  if (e.target.parentNode.parentNode.parentNode.classList.contains("card")) {
+    stored = "articles";
     document
       .querySelector(".btn-more-buy")
-      .addEventListener("click", (e) => addToCart(e));
-}
+      .addEventListener("click", () =>
+        addToCart(e.target.parentNode.parentNode.parentNode.id)
+      );
+  }
 
-function cartList() {
-  let html = "";
+  /* stored === "" && stored === "cart.articles"; */
+  Array.from(document.querySelectorAll(".print-option")).forEach((select) =>
+    select.addEventListener("change", () => {
+      calcPrice({
+        id:
+          stored === "articles"
+            ? e.target.parentNode.parentNode.parentNode.id
+            : select.parentNode.parentNode.id,
 
-  cart.articles.sort((a, b) => {
-    if (a.title > b.title) {
-      return 1;
-    } else if (a.title < b.title) {
-      return -1;
-    } else {
-      return 0;
-    }
-  });
-
-  cart.articles.length > 0 &&
-    cart.articles.forEach((article) => {
-      html += `<li id="${article.id}"><div class="img-container"><img src="${
-        article.img
-      }"></div><h3 class="article-title">${casedWords(article.title)}</h3><div>
-      <label for="size">Size</label><select name="size" id="size">
-      <option value="big">70x90</option>
-      <option value="medium">50x70</option>
-      <option value="small">30x20</option>
-    </select></div>
-    <div>
-    <label for="finish">Finish</label>
-    <select name="paper" id="paper">
-      <option value="matte">Matte</option>
-      <option value="glossy">Glossy</option>
-    </select></div><p class="article-price">${
-      article.price
-    } kr</p><i class="bi bi-trash"></i></li>`;
-    });
-  return html;
-}
-
-function calcTotal() {
-  let sum = 0;
-  if (cart.articles.length > 0)
-    cart.articles.forEach((article) => (sum += article.price));
-  return sum;
+        choosen: select.value,
+        /* arrayStored: stored, */
+      });
+    })
+  );
 }
 
 function modalFilling(e) {
@@ -192,15 +232,21 @@ function modalFilling(e) {
     <div class="more-info">
       <h3>${casedWords(articles[id].title)}</h3>
       <label for="size">Size</label>
-      <select name="size" id="size">
-        <option value="big">70x90</option>
-        <option value="medium">50x70</option>
-        <option value="small">30x20</option>
+      <select name="size" id="size" class="print-option">
+      <option value="big">70x90</option>
+      <option value="medium" ${
+        articles[id].size === "medium" ? "selected" : ""
+      }>50x70</option>
+      <option value="small" ${
+        articles[id].size === "small" ? "selected" : ""
+      }>30x20</option>
       </select>
       <label for="finish">Finish</label>
-      <select name="paper" id="paper">
-        <option value="matte">Matte</option>
-        <option value="glossy">Glossy</option>
+      <select name="paper" id="paper" class="print-option">
+      <option value="matte" >Matte</option>
+      <option value="glossy" ${
+        articles[id].finish === "glossy" ? "selected" : ""
+      }>Glossy</option>
       </select>
       <small>Price</small>
       <p class="price">${articles[id].price} kr</p>
@@ -208,14 +254,14 @@ function modalFilling(e) {
     </div>
   </div>`;
   } else if (e.target.classList.contains("bi-bag")) {
-    html = `<div class="cart">
+    html = `<div class="cart"><i class="bi bi-x"></i>
     <div class="cart-holder">
       <ul class="cart-list">
       ${cartList()}
       </ul>
       </div>
       <div class="summary">
-        <h3>Total</h3><p>${calcTotal()} kr</p>
+        <h3>Total</h3><p class="price-total">${calcTotal()} kr</p>
       </div>
       <button class="toCheckout" >Proceed to Checkout</button>
   </div>`;
@@ -239,26 +285,132 @@ document.querySelector("body").addEventListener("click", (e) => {
     document.body.style.position = "";
     /*  document.body.style.top = ""; */
   } else if (e.target.classList.contains("bi-trash")) {
-    console.log(e.target.parentNode.id);
+    deleteFromCart(e.target);
   }
 });
 
-function addToCart(e) {
-  const id = e.target.parentNode.parentNode.parentNode.id;
-  cart.articles.push(articles.find((article) => Number(article.id) == id));
+function deleteFromCart(el) {
+  cart.articles.splice(
+    Number(cart.articles.findIndex((art) => art.cartId == el.parentNode.id)),
+    1
+  );
+  el.parentNode.parentNode.removeChild(el.parentNode);
+  document.querySelector(".price-total").innerText = calcTotal();
+  if (cart.articles.length === 0) {
+    document.querySelector(".cart-holder").innerHTML = emptyCartMessage();
+  }
+  displayCartCount();
+}
+
+function calcPrice(arg) {
+  const { id, choosen } = arg;
+
+  let article;
+
+  if (Number(id) >= 100) {
+    article = cart.articles.find((article) => Number(article.cartId) == id);
+  } else {
+    article = articles.find((article) => Number(article.id) == id);
+  }
+
+  const printConfig = {
+    finish: {
+      matte: 0,
+      glossy: 300,
+    },
+    size: {
+      big: 0,
+      medium: -250,
+      small: -500,
+    },
+  };
+
+  const sizePriceChange = 250;
+  const finishPriceChange = 300;
+
+  if (choosen === "glossy" || choosen === "matte") {
+    if (article.finish === "matte" && choosen === "glossy") {
+      article.price += finishPriceChange;
+      console.log(choosen);
+    }
+    if (article.finish === "glossy" && choosen === "matte") {
+      article.price -= finishPriceChange;
+    }
+    article.finish = choosen;
+  } else {
+    if (article.size === "big" && choosen === "small") {
+      console.log(choosen);
+      article.price -= sizePriceChange * 2;
+      console.log(sizePriceChange * 2);
+    }
+    if (article.size === "big" && choosen === "medium") {
+      article.price -= sizePriceChange;
+    }
+    if (article.size === "medium" && choosen === "big") {
+      article.price += sizePriceChange;
+    }
+    if (article.size === "medium" && choosen === "small") {
+      article.price -= sizePriceChange;
+    }
+    if (article.size === "small" && choosen === "big") {
+      article.price += sizePriceChange * 2;
+    }
+    if (article.size === "small" && choosen === "medium") {
+      article.price += sizePriceChange;
+    }
+    article.size = choosen;
+  }
+  console.log(article.price);
+  displayNewPrice(article);
+}
+
+function displayNewPrice(article) {
+  console.log(article.cartId);
+  if (article.cartId) {
+    document
+      .getElementById(article.cartId)
+      .querySelector(".article-price").innerText = `${article.price} kr`;
+  } else {
+    document.querySelector(".price").innerText = `${article.price} kr`;
+  }
+
+  document.querySelector(".price-total").innerText = `${calcTotal()} kr`;
+}
+
+function addToCart(id) {
+  const finish = document.getElementById("paper");
+  const size = document.getElementById("size");
+
+  let boughtArticle = articles.find((article) => Number(article.id) == id);
+
+  size ? (boughtArticle.size = size.value) : (boughtArticle.size = "big");
+  finish
+    ? (boughtArticle.finish = finish.value)
+    : (boughtArticle.finish = "matte");
+
+  cart.articles.length = 0
+    ? (boughtArticle.cartId = Number(id) + 100)
+    : cart.articles.max();
+
+  cart.articles.push(boughtArticle);
+
   localStorage.setItem("cart", JSON.stringify(cart));
   displayCartCount();
 }
 
 function displayCartCount() {
   const counterHTML = document.querySelector(".cart-count");
-  if (counterHTML) {
-    counterHTML.innerHTML = cart.articles.length;
+  if (cart.articles.length > 0) {
+    if (counterHTML) {
+      counterHTML.innerHTML = cart.articles.length;
+    } else {
+      const counter = `<p class="cart-count">${cart.articles.length}</p>`;
+      document
+        .querySelector(".cart-link")
+        .insertAdjacentHTML("beforeend", counter);
+    }
   } else {
-    const counter = `<p class="cart-count">${cart.articles.length}</p>`;
-    document
-      .querySelector(".cart-link")
-      .insertAdjacentHTML("beforeend", counter);
+    counterHTML.parentNode.removeChild(counterHTML);
   }
 }
 
